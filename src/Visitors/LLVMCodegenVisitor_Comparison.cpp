aea4d76903e774d5126a172e8e5f56897f960548
Ghavvvo <2gaherre12@gmail.com>
2025-06-15 03:20:18 -0400

    Fix decimals comparisons op

--- src/Visitors/LLVMCodegenVisitor_Comparison.cpp
+++ src/Visitors/LLVMCodegenVisitor_Comparison.cpp
@@ -9,26 +9,78 @@
 #include <llvm/IR/Constants.h>
 #include <llvm/IR/Type.h>
 
+
+static std::pair<llvm::Value*, llvm::Value*> convertToCompatibleTypes(
+    llvm::Value* l, llvm::Value* r, llvm::IRBuilder<>& builder) {
+    
+    if (l->getType() == r->getType()) {
+        return {l, r};
+    }
+    
+
+    if (l->getType()->isIntegerTy() && r->getType()->isIntegerTy()) {
+        if (l->getType()->getIntegerBitWidth() < r->getType()->getIntegerBitWidth()) {
+            l = builder.CreateZExt(l, r->getType(), "conv_to_larger_int");
+        } else if (r->getType()->getIntegerBitWidth() < l->getType()->getIntegerBitWidth()) {
+            r = builder.CreateZExt(r, l->getType(), "conv_to_larger_int");
+        }
+        return {l, r};
+    }
+    
+
+    if ((l->getType()->isIntegerTy() && r->getType()->isDoubleTy()) ||
+        (l->getType()->isDoubleTy() && r->getType()->isIntegerTy())) {
+        
+        if (l->getType()->isIntegerTy()) {
+            l = builder.CreateSIToFP(l, llvm::Type::getDoubleTy(builder.getContext()), "int_to_double");
+        }
+        if (r->getType()->isIntegerTy()) {
+            r = builder.CreateSIToFP(r, llvm::Type::getDoubleTy(builder.getContext()), "int_to_double");
+        }
+        return {l, r};
+    }
+    
+
+    if (l->getType()->isDoubleTy() && r->getType()->isDoubleTy()) {
+        return {l, r};
+    }
+    
+
+    if (!l->getType()->isIntegerTy()) {
+        if (l->getType()->isDoubleTy()) {
+            l = builder.CreateFPToSI(l, llvm::Type::getInt32Ty(builder.getContext()), "double_to_int");
+        } else {
+            l = builder.CreatePtrToInt(l, llvm::Type::getInt32Ty(builder.getContext()), "ptr_to_int");
+        }
+    }
+    if (!r->getType()->isIntegerTy()) {
+        if (r->getType()->isDoubleTy()) {
+            r = builder.CreateFPToSI(r, llvm::Type::getInt32Ty(builder.getContext()), "double_to_int");
+        } else {
+            r = builder.CreatePtrToInt(r, llvm::Type::getInt32Ty(builder.getContext()), "ptr_to_int");
+        }
+    }
+    
+    return {l, r};
+}
+
 void LLVMCodegenVisitor::visit(EqualNode& node) {
     node.left->accept(*this);
     llvm::Value* l = lastValue;
     node.right->accept(*this);
     llvm::Value* r = lastValue;
+
+
+    auto converted = convertToCompatibleTypes(l, r, builder);
+    l = converted.first;
+    r = converted.second;
     
 
-    if (l->getType() != r->getType()) {
-        if (l->getType()->isIntegerTy() && r->getType()->isIntegerTy()) {
-
-            if (l->getType()->getIntegerBitWidth() < r->getType()->getIntegerBitWidth()) {
-                l = builder.CreateZExt(l, r->getType(), "conv_to_larger_int");
-            } else if (r->getType()->getIntegerBitWidth() < l->getType()->getIntegerBitWidth()) {
-                r = builder.CreateZExt(r, l->getType(), "conv_to_larger_int");
-            }
-        }
+    if (l->getType()->isDoubleTy()) {
+        lastValue = builder.CreateFCmpOEQ(l, r, "eqtmp");
+    } else {
+        lastValue = builder.CreateICmpEQ(l, r, "eqtmp");
     }
-    
-
-    lastValue = builder.CreateICmpEQ(l, r, "eqtmp");
 }
 
 void LLVMCodegenVisitor::visit(NotEqualNode& node) {
@@ -36,21 +88,18 @@ void LLVMCodegenVisitor::visit(NotEqualNode& node) {
     llvm::Value* l = lastValue;
     node.right->accept(*this);
     llvm::Value* r = lastValue;
+
+
+    auto converted = convertToCompatibleTypes(l, r, builder);
+    l = converted.first;
+    r = converted.second;
     
 
-    if (l->getType() != r->getType()) {
-        if (l->getType()->isIntegerTy() && r->getType()->isIntegerTy()) {
-
-            if (l->getType()->getIntegerBitWidth() < r->getType()->getIntegerBitWidth()) {
-                l = builder.CreateZExt(l, r->getType(), "conv_to_larger_int");
-            } else if (r->getType()->getIntegerBitWidth() < l->getType()->getIntegerBitWidth()) {
-                r = builder.CreateZExt(r, l->getType(), "conv_to_larger_int");
-            }
-        }
+    if (l->getType()->isDoubleTy()) {
+        lastValue = builder.CreateFCmpONE(l, r, "netmp");
+    } else {
+        lastValue = builder.CreateICmpNE(l, r, "netmp");
     }
-    
-
-    lastValue = builder.CreateICmpNE(l, r, "netmp");
 }
 
 void LLVMCodegenVisitor::visit(GreaterNode& node) {
@@ -58,21 +107,18 @@ void LLVMCodegenVisitor::visit(GreaterNode& node) {
     llvm::Value* l = lastValue;
     node.right->accept(*this);
     llvm::Value* r = lastValue;
+
+
+    auto converted = convertToCompatibleTypes(l, r, builder);
+    l = converted.first;
+    r = converted.second;
     
 
-    if (l->getType() != r->getType()) {
-        if (l->getType()->isIntegerTy() && r->getType()->isIntegerTy()) {
-
-            if (l->getType()->getIntegerBitWidth() < r->getType()->getIntegerBitWidth()) {
-                l = builder.CreateZExt(l, r->getType(), "conv_to_larger_int");
-            } else if (r->getType()->getIntegerBitWidth() < l->getType()->getIntegerBitWidth()) {
-                r = builder.CreateZExt(r, l->getType(), "conv_to_larger_int");
-            }
-        }
+    if (l->getType()->isDoubleTy()) {
+        lastValue = builder.CreateFCmpOGT(l, r, "gttmp");
+    } else {
+        lastValue = builder.CreateICmpSGT(l, r, "gttmp");
     }
-    
-
-    lastValue = builder.CreateICmpSGT(l, r, "gttmp");
 }
 
 void LLVMCodegenVisitor::visit(GreaterEqNode& node) {
@@ -80,21 +126,18 @@ void LLVMCodegenVisitor::visit(GreaterEqNode& node) {
     llvm::Value* l = lastValue;
     node.right->accept(*this);
     llvm::Value* r = lastValue;
+
+
+    auto converted = convertToCompatibleTypes(l, r, builder);
+    l = converted.first;
+    r = converted.second;
     
 
-    if (l->getType() != r->getType()) {
-        if (l->getType()->isIntegerTy() && r->getType()->isIntegerTy()) {
-
-            if (l->getType()->getIntegerBitWidth() < r->getType()->getIntegerBitWidth()) {
-                l = builder.CreateZExt(l, r->getType(), "conv_to_larger_int");
-            } else if (r->getType()->getIntegerBitWidth() < l->getType()->getIntegerBitWidth()) {
-                r = builder.CreateZExt(r, l->getType(), "conv_to_larger_int");
-            }
-        }
+    if (l->getType()->isDoubleTy()) {
+        lastValue = builder.CreateFCmpOGE(l, r, "getmp");
+    } else {
+        lastValue = builder.CreateICmpSGE(l, r, "getmp");
     }
-    
-
-    lastValue = builder.CreateICmpSGE(l, r, "getmp");
 }
 
 void LLVMCodegenVisitor::visit(LessNode& node) {
@@ -102,21 +145,18 @@ void LLVMCodegenVisitor::visit(LessNode& node) {
     llvm::Value* l = lastValue;
     node.right->accept(*this);
     llvm::Value* r = lastValue;
+
+
+    auto converted = convertToCompatibleTypes(l, r, builder);
+    l = converted.first;
+    r = converted.second;
     
 
-    if (l->getType() != r->getType()) {
-        if (l->getType()->isIntegerTy() && r->getType()->isIntegerTy()) {
-
-            if (l->getType()->getIntegerBitWidth() < r->getType()->getIntegerBitWidth()) {
-                l = builder.CreateZExt(l, r->getType(), "conv_to_larger_int");
-            } else if (r->getType()->getIntegerBitWidth() < l->getType()->getIntegerBitWidth()) {
-                r = builder.CreateZExt(r, l->getType(), "conv_to_larger_int");
-            }
-        }
+    if (l->getType()->isDoubleTy()) {
+        lastValue = builder.CreateFCmpOLT(l, r, "lttmp");
+    } else {
+        lastValue = builder.CreateICmpSLT(l, r, "lttmp");
     }
-    
-
-    lastValue = builder.CreateICmpSLT(l, r, "lttmp");
 }
 
 void LLVMCodegenVisitor::visit(LessEqNode& node) {
@@ -124,19 +164,16 @@ void LLVMCodegenVisitor::visit(LessEqNode& node) {
     llvm::Value* l = lastValue;
     node.right->accept(*this);
     llvm::Value* r = lastValue;
+
+
+    auto converted = convertToCompatibleTypes(l, r, builder);
+    l = converted.first;
+    r = converted.second;
     
 
-    if (l->getType() != r->getType()) {
-        if (l->getType()->isIntegerTy() && r->getType()->isIntegerTy()) {
-
-            if (l->getType()->getIntegerBitWidth() < r->getType()->getIntegerBitWidth()) {
-                l = builder.CreateZExt(l, r->getType(), "conv_to_larger_int");
-            } else if (r->getType()->getIntegerBitWidth() < l->getType()->getIntegerBitWidth()) {
-                r = builder.CreateZExt(r, l->getType(), "conv_to_larger_int");
-            }
-        }
+    if (l->getType()->isDoubleTy()) {
+        lastValue = builder.CreateFCmpOLE(l, r, "letmp");
+    } else {
+        lastValue = builder.CreateICmpSLE(l, r, "letmp");
     }
-    
-
-    lastValue = builder.CreateICmpSLE(l, r, "letmp");
 }
--- src/Visitors/LLVMCodegenVisitor_Logical.cpp
+++ src/Visitors/LLVMCodegenVisitor_Logical.cpp
@@ -13,8 +13,28 @@ void LLVMCodegenVisitor::visit(AndNode& node) {
     node.right->accept(*this);
     llvm::Value* r = lastValue;
     
-    l = builder.CreateICmpNE(l, llvm::ConstantInt::get(l->getType(), 0), "andl");
-    r = builder.CreateICmpNE(r, llvm::ConstantInt::get(r->getType(), 0), "andr");
+
+    if (l->getType()->isIntegerTy()) {
+        l = builder.CreateICmpNE(l, llvm::ConstantInt::get(l->getType(), 0), "andl");
+    } else if (l->getType()->isDoubleTy()) {
+        l = builder.CreateFCmpONE(l, llvm::ConstantFP::get(l->getType(), 0.0), "andl");
+    } else {
+
+        llvm::Value* intVal = builder.CreatePtrToInt(l, llvm::Type::getInt32Ty(builder.getContext()), "ptrtoint");
+        l = builder.CreateICmpNE(intVal, llvm::ConstantInt::get(llvm::Type::getInt32Ty(builder.getContext()), 0), "andl");
+    }
+    
+
+    if (r->getType()->isIntegerTy()) {
+        r = builder.CreateICmpNE(r, llvm::ConstantInt::get(r->getType(), 0), "andr");
+    } else if (r->getType()->isDoubleTy()) {
+        r = builder.CreateFCmpONE(r, llvm::ConstantFP::get(r->getType(), 0.0), "andr");
+    } else {
+
+        llvm::Value* intVal = builder.CreatePtrToInt(r, llvm::Type::getInt32Ty(builder.getContext()), "ptrtoint");
+        r = builder.CreateICmpNE(intVal, llvm::ConstantInt::get(llvm::Type::getInt32Ty(builder.getContext()), 0), "andr");
+    }
+    
     lastValue = builder.CreateAnd(l, r, "andtmp");
 }
 
@@ -24,15 +44,46 @@ void LLVMCodegenVisitor::visit(OrNode& node) {
     node.right->accept(*this);
     llvm::Value* r = lastValue;
     
-    l = builder.CreateICmpNE(l, llvm::ConstantInt::get(l->getType(), 0), "orl");
-    r = builder.CreateICmpNE(r, llvm::ConstantInt::get(r->getType(), 0), "orr");
+
+    if (l->getType()->isIntegerTy()) {
+        l = builder.CreateICmpNE(l, llvm::ConstantInt::get(l->getType(), 0), "orl");
+    } else if (l->getType()->isDoubleTy()) {
+        l = builder.CreateFCmpONE(l, llvm::ConstantFP::get(l->getType(), 0.0), "orl");
+    } else {
+
+        llvm::Value* intVal = builder.CreatePtrToInt(l, llvm::Type::getInt32Ty(builder.getContext()), "ptrtoint");
+        l = builder.CreateICmpNE(intVal, llvm::ConstantInt::get(llvm::Type::getInt32Ty(builder.getContext()), 0), "orl");
+    }
+    
+
+    if (r->getType()->isIntegerTy()) {
+        r = builder.CreateICmpNE(r, llvm::ConstantInt::get(r->getType(), 0), "orr");
+    } else if (r->getType()->isDoubleTy()) {
+        r = builder.CreateFCmpONE(r, llvm::ConstantFP::get(r->getType(), 0.0), "orr");
+    } else {
+
+        llvm::Value* intVal = builder.CreatePtrToInt(r, llvm::Type::getInt32Ty(builder.getContext()), "ptrtoint");
+        r = builder.CreateICmpNE(intVal, llvm::ConstantInt::get(llvm::Type::getInt32Ty(builder.getContext()), 0), "orr");
+    }
+    
     lastValue = builder.CreateOr(l, r, "ortmp");
 }
 
 void LLVMCodegenVisitor::visit(NotNode& node) {
     node.operand->accept(*this);
     llvm::Value* v = lastValue;
-    v = builder.CreateICmpEQ(v, llvm::ConstantInt::get(v->getType(), 0), "nottmp");
+    
+
+    if (v->getType()->isIntegerTy()) {
+        v = builder.CreateICmpEQ(v, llvm::ConstantInt::get(v->getType(), 0), "nottmp");
+    } else if (v->getType()->isDoubleTy()) {
+        v = builder.CreateFCmpOEQ(v, llvm::ConstantFP::get(v->getType(), 0.0), "nottmp");
+    } else {
+
+        llvm::Value* intVal = builder.CreatePtrToInt(v, llvm::Type::getInt32Ty(builder.getContext()), "ptrtoint");
+        v = builder.CreateICmpEQ(intVal, llvm::ConstantInt::get(llvm::Type::getInt32Ty(builder.getContext()), 0), "nottmp");
+    }
+    
     lastValue = v;
 }
 
